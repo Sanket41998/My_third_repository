7e5ecf9a8796f66fe92b58fbb033ce50dde7705c
Sanket41998 <sanketwaikar5@gmail.com>
2024-01-01 19:21:54 +0530

    my third commit

--- hello_world.py
+++ hello_world.py
@@ -7,3 +7,19 @@ a=20
 b=10
 c=a-b
 print(c)
+
+class India:
+    def capital(self):
+        print("new delhi is capital of India")
+    def language(self):
+        print("hindi is spoken widely")
+class USA:
+    def capital(self):
+        print("Washigton DC is the capital of USA")
+    def language(self):
+        print("English is spoken widely")
+obj1=India()
+obj2=USA()
+for i in (obj1,obj2):
+    i.capital()
+    i.language()
