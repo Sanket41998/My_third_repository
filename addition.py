8f6b6746b98e51045d162f771a2622d27b2c702e
Sanket41998 <sanketwaikar5@gmail.com>
2024-01-01 19:32:57 +0530

    this si my second commit

--- addition.py
+++ addition.py
@@ -2,3 +2,13 @@ a=20
 b=40
 c=a+b
 print(c)
+
+class name:
+    def first_name(self):
+        print("sanket")
+class name2(name):
+    def last_name(self):
+        print("waikar")
+obj_f=name2()
+obj_f.first_name()
+obj_f.last_name()
